ee242bb14a14f9a92e1b7ab5e24a9a9e3a6c2c5b
RetepMil <lkslyj2@naver.com>
2024-01-13 16:23:10 +0900

    initialize Express server

---
+++ src/server.js
@@ -0,0 +1,52 @@
+"strict mode";
+
+require("dotenv").config();
+
+const express = require("express");
+const moment = require("moment");
+const mongoose = require("mongoose");
+const app = express();
+
+const { PORT, MONGO_BASE_URI, DB_NAME } = process.env;
+
+mongoose
+  .connect(`${MONGO_BASE_URI}/${DB_NAME}`)
+  .then(() => console.log("MongoDB 접속 완료"))
+  .catch((e) => console.error(e));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 시간 측정
+app.use((req, _, next) => {
+  if (req.path === "/health") next();
+  else {
+    req.startTime = moment();
+    console.log(`요청 인입 시간 : ${req.startTime}`);
+    next();
+  }
+});
+
+app.get("/health", (req, res, next) => {
+  res.status(200).json({
+    data: {
+      message: "OK",
+    },
+  });
+  next();
+});
+
+app.use((req, _, next) => {
+  if (req.startTime) {
+    console.log(
+      `처리에 걸린 시간 : ${
+        moment.duration(moment().diff(req.startTime)).asMilliseconds() / 1000
+      }s`
+    );
+  }
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server is listening on port ${PORT}`);
+});
